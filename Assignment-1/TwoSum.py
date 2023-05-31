dd441c2be7b279b861de44cd62c1a5c0b251e903
nj6123 <117580615+nj6123@users.noreply.github.com>
2023-05-31 23:42:45 +0530

    Create TwoSum.py

---
+++ Assignment-1/TwoSum.py
@@ -0,0 +1,15 @@
+# Two Numbers Sum
+
+class Solution(object):
+    def twoSum(self, nums, target):
+         
+        required = {}
+        for i in range(len(nums)):
+            if target - nums[i] in required:
+                return [required[target - nums[i]],i]
+            else:
+                required[nums[i]]=i
+
+input_list = [2,7,11,15]
+obj = Solution()
+print(obj.twoSum(input_list, 9))
